0b0b0c1ffa3a9c81971f30d35f579db6a65e3eeb
Loïc Vanderschooten <loicvanderschooten@air-de-loic.home>
2022-12-08 08:43:20 +0100

    add header

---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+    import ButtonParticle from '../../particles/button/ButtonParticle.vue';
+    const menuItems: {name: string, link:string}[] = [
+        {
+            'name': 'Ressources',
+            'link': '/ressources'
+        },
+        {
+            'name': 'Services',
+            'link': '/services'
+        },
+        {
+            'name': 'Portfolio',
+            'link': '/porfolio'
+        },
+        {
+            'name': 'A propos',
+            'link': '/about'
+        },
+        {
+            'name': 'Contact',
+            'link': '/contact'
+        },
+    ]
+</script>
+
+<template>
+    <header>
+        <div class="classic-container">
+            <div class="menu">
+                <div class="m-logo">
+                    <img src="src/assets/img/branding/logo.svg" />
+                </div>
+                <ul class="m-items">
+                    <li class="mi-item" v-for="(item, index) in menuItems" :key="index">
+                        <a :href="item.name">{{ item.name }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="actions">
+                <ButtonParticle level="2" color="primary" label="Travailler avec nous"/>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+    @use './HeaderComponent';
+</style>
---
+++ src/particles/button/ButtonParticle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    const props = defineProps({
+        level:{type:String, required:false, default:"1"},
+        color:{type:String, required:false, default:"primary"},
+        label:{type:String, required:false, default:"Label"},
+    });
+</script>
+
+<template>
+    <button :class="`f-btn level-${level}-${color}`">
+        {{ label }}
+    </button>
+</template>
